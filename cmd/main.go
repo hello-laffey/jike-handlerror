819b789faed0563219d09b571ac3ae440d976b61
hello-laffey <wxloviky@gmail.com>
2022-05-21 16:20:05 +0800

    [MOD] 修改对sql.ErrNoRows 的处理

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,26 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"handlerror/internal/service/dao"
 	"os"
+
+	"github.com/pkg/errors"
 )
 
 // main 主函数
 // 程序的顶部，使用%+v把堆栈打清楚
 func main() {
 	if err := dao.QuerySql(); err != nil {
-		fmt.Printf("FATAL: %+v\n", err)
-		os.Exit(1)
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Printf("Warning: %v\n", err)
+			return
+		} else {
+			fmt.Printf("FATAL: %+v\n", err)
+			os.Exit(1)
+		}
 	}
 	fmt.Printf("Process finish ok\n")
+	return
 }
